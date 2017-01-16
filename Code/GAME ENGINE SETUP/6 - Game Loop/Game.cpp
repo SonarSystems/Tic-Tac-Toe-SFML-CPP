8fc1e0e61d7c1c2866ac206d4e1107950cbd9810
Sonar Systems <sonarsystems@frahaans-imac.home>
2017-01-16 17:26:32 +0000

    Reorganisation

--- Code/GAME ENGINE SETUP/6 - Game Loop/Game.cpp
+++ Code/GAME ENGINE SETUP/6 - Game Loop/Game.cpp
@@ -1,12 +1,12 @@
 #include "Game.hpp"
-#include "SplashState.hpp"
+//#include "SplashState.hpp"
 
 namespace Sonar
 {
 	Game::Game(int width, int height, std::string title)
 	{
 		_data->window.create(sf::VideoMode(width, height), title, sf::Style::Close | sf::Style::Titlebar);
-		_data->machine.AddState(StateRef(new SplashState(this->_data)));
+		//_data->machine.AddState(StateRef(new SplashState(this->_data)));
 
 		this->Run();
 	}
--- Code/GAME ENGINE SETUP/6 - Input Manager/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Game.hpp"
-
-int main()
-{
-	Sonar::Game(800, 600, "SFML Starter");
-}
